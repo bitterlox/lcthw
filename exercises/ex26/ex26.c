4bbdaec45466694c698f26d831def5bf2b6be550
angel <angel@mycos.local>
2026-01-15 16:55:35 +0100

    ex26: loding config and parsing into slice;

--- exercises/ex26/ex26.c
+++ exercises/ex26/ex26.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/stat.h>
+#include <wordexp.h>
+#include <string.h>
 
 #include "dbg.h"
 
@@ -18,16 +20,34 @@
  *
  */
 
-char *open_file(const char *path, int max_size, char *ptr)
+
+const int MAX_CONFIG = 10;
+
+void release_file(char *file)
+{
+  if (file) free(file);
+}
+
+char *open_file(const char *path)
 {
   FILE *file;
   int code;
   struct stat stat_res;
+  wordexp_t expanded;
 
-  code = stat(path, &stat_res);
-  check(code == 0, "error calling stat on path: %s\n", path)
+  code =  wordexp(path, &expanded, 0);
+  check(code == 0, "error expanding path: %s\n", path)
 
-  file = fopen(path, "r");
+  for(int i = 0; i < expanded.we_wordc; i++) {
+    debug("expanded %d: %s\n", i, expanded.we_wordv[i]);
+  }
+
+  char *expanded_path = expanded.we_wordv[0];
+
+  code = stat(expanded_path, &stat_res);
+  check(code == 0, "error calling stat on path: %s\n", expanded_path)
+
+  file = fopen(expanded_path, "r");
 
   printf("file size: %lld \n", stat_res.st_size);
 
@@ -37,11 +57,15 @@ char *open_file(const char *path, int max_size, char *ptr)
 
   buf[stat_res.st_size * sizeof(char)] = '\0';
 
-  int read = fread(buf, stat_res.st_size, 1, file);
-  check(code == 0, "error reading from file: %s\n", path)
+  // ignoring return value from fread
+  fread(buf, stat_res.st_size, 1, file);
+  check(code == 0, "error reading from file: %s\n", expanded_path)
 
   code = fclose(file);
-  check(code == 0, "error closing file: %s\n", path)
+  check(code == 0, "error closing file: %s\n", expanded_path)
+
+  wordfree(&expanded);
+
 
   return buf;
 
@@ -50,30 +74,82 @@ char *open_file(const char *path, int max_size, char *ptr)
     return NULL;
 }
 
-void release_file(char *file)
+void release_config(char **config)
 {
-  if (file) free(file);
+  for (int i = 0; i < MAX_CONFIG; i++) {
+    if (config[i]) {
+      free(config[i]);
+    }
+  }
+  free(config);
 }
+
+char **load_config(const char *path)
+{
+  char *raw_config = open_file(path);
+  check(raw_config, "config file is null");
+
+  int count = 0;
+  char *parsed;
+  char *to_parse = raw_config;
+
+  char **config = malloc(MAX_CONFIG * sizeof(char *));
+  check_mem(config);
+
+  do {
+    check(count + 1 < MAX_CONFIG, "max number of config values allowed is %d", MAX_CONFIG);
+
+
+    // this is the wierdest function ever
+    // apparently it has a hidden pointer it uses in subsequent calls
+    // to keep parsing
+    parsed = strtok(to_parse, "\n");
+
+    if (!parsed) {
+      break;
+    }
+
+    config[count] = strdup(parsed);
+
+    to_parse = NULL;
+
+    count++;
+  } while(1==1);
+
+  release_file(raw_config);
+
+  return config;
+
+error:
+  printf("couldn't load config\n");
+  release_file(raw_config);
+  release_config(config);
+
+  return NULL;
+}
+
 
 int main(int argc, char *argv[])
 {
-  printf("%s\n", "hello world!");
+  char **config = load_config("~/.logfind");
+  check(config, "config is null");
 
-  char *content = open_file("test.txt", 0, NULL);
-  check(content, "content is null")
 
-  printf("%s\n", content);
+  for (int i = 0; i < MAX_CONFIG && config[i]; i ++) {
+    printf("config %d: %s\n", i, config[i]);
+  }
+
+  release_config(config);
+
 
   // char **content1 = open_file("test1.txt", 0, NULL);
   // check(content1, "content is null")
 
 
-  release_file(content);
-
   return 0;
 
   error:
-    release_file(content);
+    // release_file(content);
     return -1;
 }
 
